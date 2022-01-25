ed7f74ee6cf35a447ad6640f8a22233c475f7e8f
Clare-Joyce <joycetirnyuy@gmail.com>
2022-01-25 17:31:51 +0300

    Change the car speed and modify comments.

--- python_tonny_gaddis/chapter02/Ex_5_distance_calculation.py
+++ python_tonny_gaddis/chapter02/Ex_5_distance_calculation.py
@@ -1,10 +1,10 @@
-#display distance covered in specific time period
+# Display distance covered in specific time period.
 
-#car speed in miles per hour
-speed = 70
+# Car speed in miles per hour
+speed = 60
 
-#distance  = speed *time
-#distnce covered
+# Distance  = speed *t ime
+# Distance covered
 print("Distance covered in 6 hours ", 6*speed)
 print("Distance covered in 10 hours ", 10*speed)
 print("Distance covered in 15 hours ", 15*speed)
