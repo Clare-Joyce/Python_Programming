8c851ef2a81759b2a56fd751905caee8ef6cf97a
Clare-Joyce <joycetirnyuy@gmail.com>
2022-01-25 17:39:32 +0300

    Modify the structure of the comments.

--- python_tonny_gaddis/chapter02/Ex_6_sales_tax.py
+++ python_tonny_gaddis/chapter02/Ex_6_sales_tax.py
@@ -1,30 +1,29 @@
-# The program should then compute the state and county sales tax. 
-# state sales tax is 5 percent 
-# county sales tax is 2.5 percent.
-# display the amount of the pur-chase, 
-# the state sales tax, 
-# the county sales tax;
-# the total sales tax, 
-# the total of the sale 
+"""The program should then compute the state and county sales tax. 
+- state sales tax is 5 percent 
+- county sales tax is 2.5 percent.
+- display the amount of the pur-chase, 
+- the state sales tax, 
+- the county sales tax;
+- the total sales tax, 
+- the total of the sale """
 
-#initalize variables
+# Initalize variables
 state_sales_tax_percent = 5
-county_sales_tax_percent - 2.5
+county_sales_tax_percent = 2.5
 
-
-#prmpt user for amount of purchase
-amount_purchase = float(input("enter amount of purchase: "))
-#calculate state sales tax
-state_sales_tax = amount_purchase * state_sales_tax_percent / 100
-#calculate the county sales tax
-county_sales_tax = amount_purchase * county_sales_tax_percent / 100 
-#calculate the total sales tax
+# Prompt user for amount of purchase
+purchase_amount = float(input("enter amount of purchase: "))
+# Calculate state sales tax
+state_sales_tax = purchase_amount * state_sales_tax_percent / 100
+# Calculate the county sales tax
+county_sales_tax = purchase_amount * county_sales_tax_percent / 100 
+# Calculate the total sales tax
 total_sales_tax = state_sales_tax + county_sales_tax
-#calculate  total sale
-total_sales = amount_purchase + total_sales_tax
+# Calculate  total sale
+total_sales = purchase_amount + total_sales_tax
 
-#display these values
-print("Amount purchase: ", amount_purchase)
+# Display these values
+print("Amount purchase: ", purchase_amount)
 print("State sales tax: ", state_sales_tax)
 print("County sales tax: ", county_sales_tax)
 print("Total sales tax: ", total_sales_tax)
