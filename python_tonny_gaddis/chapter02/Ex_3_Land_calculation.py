7cbdb8d8ab0588c42000a1164b8266ce05c45e20
Clare-Joyce <joycetirnyuy@gmail.com>
2022-01-25 16:21:04 +0300

    Use f printing to display.

--- python_tonny_gaddis/chapter02/Ex_3_Land_calculation.py
+++ python_tonny_gaddis/chapter02/Ex_3_Land_calculation.py
@@ -1,9 +1,9 @@
-#conversion from sqaure feets to acres
-#1 acres = 43560 square feet
+# Conversion from sqaure feets to acres
+# 1 acre = 43560 square feet
 Square_feet_per_acre = 43560
-#prompt user for number of square feets
-square_feets = int(input("Enter the number of square_feetsin the land. "))
-#calcculate the corresponding nummber of acres
+# Prompt user for number of square feets
+square_feets = int(input("Enter the number of square_feetsin the land: "))
+# Calculate the corresponding nummber of acres
 acres = square_feets/Square_feet_per_acre
-#display the value
-print("Equivalent number of acres is ", acres)
+# Display the value
+print(f"Equivalent number of acres is {acres}")
