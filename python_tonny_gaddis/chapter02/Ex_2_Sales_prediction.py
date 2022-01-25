000fdd6ef8bed03dd5021fecef91eff28f34d12e
Clare-Joyce <joycetirnyuy@gmail.com>
2022-01-25 16:04:01 +0300

    Reduce the lines of code.

--- python_tonny_gaddis/chapter02/Ex_2_Sales_prediction.py
+++ python_tonny_gaddis/chapter02/Ex_2_Sales_prediction.py
@@ -1,14 +1,11 @@
-#predicting profit from a projected amount of total sales
+"""Predicting profit from a projected
+amount of total sales."""
 
-
-#annual profit percentage
+# Annual profit percentage
 annual_profit = 23
 
-#prompt user for projected sales
+# Prompt user for projected sales
 sales = float(input("Enter projected sales: "))
 
-#calculate the projected profit
-projected_profit = sales*annual_profit/100
-
-#display the projected profit
-print("Projected Profit = $", projected_profit)
+# Calculate and display the projected profit
+print("Projected Profit = $", sales*annual_profit/100)
