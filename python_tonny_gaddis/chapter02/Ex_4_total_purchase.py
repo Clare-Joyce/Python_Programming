f06444095fbcee427e87d689dfd6595719ae16db
Clare-Joyce <joycetirnyuy@gmail.com>
2022-01-25 17:28:22 +0300

    Check for valid input.

--- python_tonny_gaddis/chapter02/Ex_4_total_purchase.py
+++ python_tonny_gaddis/chapter02/Ex_4_total_purchase.py
@@ -1,17 +1,19 @@
-#calculate the tax and total cost of items purchased
+""" Calculate the tax and total cost of items purchased."""
 
-
-tax_percent = 7 #in %
-#initialize an accumulator
+tax_percent = 7 # in %
+# Initialize an accumulator
 total = 0.0
 for i in range(5):
 	print("Enter price for item #", i+1)
-	price = float(input("Price = "))
-	#calculate subtotal
-	subtotal=price+(tax_percent*price/100)
-	print("total for item #", i+1, "= ", subtotal)
+	try:
+		price = float(input("Price = "))
+	except  ValueError:
+		print("Invalid input. Please enter a number.")
+	# Calculate subtotal
+	subtotal = price + (tax_percent * price / 100)
+	print("total for item #", i + 1, "= ", subtotal)
 
-	total= total+subtotal
-print("Your total bill is $", total)
+	total =+ subtotal
+print("\nYour total bill is $", total)
 
 
