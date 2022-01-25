23b0b086da2cf3c1d4ec572bd2231ac6265ce008
Clare-Joyce <joycetirnyuy@gmail.com>
2022-01-25 17:49:03 +0300

    Format the total value.

--- python_tonny_gaddis/chapter02/Ex_8_food_tip_tap_total.py
+++ python_tonny_gaddis/chapter02/Ex_8_food_tip_tap_total.py
@@ -1,22 +1,20 @@
-#calculating the tip, tax and total in a restau
+""" Calculating the tip, tax and total in a restaurant."""
 
-#prompt user for cost of food
+# Prompt user for cost of food
 food_cost = float(input("Enter the cost of the food: "))
 
-#percentage tip
+# Percentage tip
 tip_percentage = 18
-#sales tax percentage
+# Sales tax percentage
 sales_tax_percentage = 7
-#calculate the tip amount
-tip = tip_percentage*food_cost/100
-#calculate the amount of sales tax
-sales_tax = sales_tax_percentage*food_cost/100
-#calculate the total
-Total = food_cost+tip+sales_tax
+# Calculate the tip amount
+tip = tip_percentage * food_cost / 100
+# Calculate the amount of sales tax
+sales_tax = sales_tax_percentage * food_cost / 100
+# Calculate the total
+total = sum([food_cost, tip, sales_tax])
 
-#display these values
-
+# Display these values
 print("Tip: ", tip)
 print("Sales Tax:", sales_tax)
-print("Total Cost:", Total)
-
+print("Total Cost:", format(total, '.2f'))
