5b6014d0b999fb2c5556a2b7d01eb1820f32140e
Clare-Joyce <joycetirnyuy@gmail.com>
2022-01-26 12:10:09 +0300

    Modify file name.

--- python_tonny_gaddis/chapter02/ex_12_Stock_transaction.py
+++ python_tonny_gaddis/chapter02/ex_12_Stock_transaction.py
@@ -1,19 +1,21 @@
+"""Stock Transaction Program"""
+
 num_share = num_sold = 2000
 cost_per_share = 40.00
-amount_paid = num_share*cost_per_share
-stock_broker_commission = amount_paid*3/100
+amount_paid = num_share * cost_per_share
+stock_broker_commission = amount_paid * 3 / 100
 selling_price_pershare = 42.75
-amount_sold = num_sold*selling_price_pershare
-stock_broker_commission2 = amount_sold*3/100
+amount_sold = num_sold * selling_price_pershare
+stock_broker_commission2 = amount_sold * 3 / 100
 
-print("Amount paid for stock $", amount_paid)
-print("Amount received by stock broker before sale #", stock_broker_commission)
+print("\nAmount paid for stock $", amount_paid)
+print("Amount received by stock broker before sale $", stock_broker_commission)
 print("The stock was sold for $", amount_sold)
 print("Amount received by stock broker after sales $", stock_broker_commission2)
 
-stock_broker = stock_broker_commission2+stock_broker_commission
+stock_broker = stock_broker_commission2 + stock_broker_commission
 amount_left = amount_sold  - stock_broker
 if amount_left > amount_paid:
-	print("profit made")
+	print("profit made\n")
 else:
-	print("No profit")
+	print("No profit\n")
